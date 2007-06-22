57d12c4100529d2fe0ee30e2ea46093efdb5bbeb
taylott <taylott@ebc2ff89-3524-0410-b27c-cb40b083701b>
2007-06-22 04:57:38 +0000

    Quick hack to fix a Ruby error when parsing the lint output. This needs a better fix as the html is all goofed up when an error doesn't have corresponding lines of code. See jsl-test.js
    git-svn-id: http://bundles.bundleforge.com/trunk/bundles/JavaScript Tools.tmbundle@12 ebc2ff89-3524-0410-b27c-cb40b083701b

--- Support/bin/lint.rb
+++ Support/bin/lint.rb
@@ -26,7 +26,7 @@ output  = output.map do |chunk|
   j = 0
   lines = lines.map do |line|
     if line =~ /^(\d+):/
-      column = (lines[j+2].rindex("^") + 1).to_s
+      column = (lines[j+2].rindex("^") + 1).to_s rescue ''
       line.gsub!(/^(\d+):/, %{<a href="txmt://open?url=file://#{FILEPATH}&line=\\1&column=#{column}">\\1</a>})
       line.gsub!('#{column}', column);
       line.gsub!('#{FILEPATH}', FILEPATH);
